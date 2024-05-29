3f522a1f5222b136b88485ede074b0a6d6189917
North-West-Wind <wsyn148@gmail.com>
2024-05-30 01:27:38 +0800

    proper reply handling

--- src/matrix/index.ts
+++ src/matrix/index.ts
@@ -23,7 +23,7 @@ client.on("room.message", async (roomId: string, event: RoomMessageEvent) => {
 	const selfId = await client.getUserId()
 	if (event.sender === selfId || event.sender === "@discord_" + discordId()) return;
 
-	const body = event.content.body;
+	let body = event.content.body;
 
 	const states = await client.getRoomState(roomId);
 	let name = states.filter(state => state.type == "m.room.name").pop()?.content.name || "";
@@ -36,10 +36,25 @@ client.on("room.message", async (roomId: string, event: RoomMessageEvent) => {
 
 	let replyToMe = false;
 	const replyEventId = (event.content["m.relates_to"] as any)?.["m.in_reply_to"]?.event_id;
-	try {
-		replyToMe = replyEventId && (await client.getEvent(roomId, replyEventId))?.sender === selfId;
-	} catch (err) {
-		console.error(err);
+	if (replyEventId) {
+		try {
+			replyToMe = (await client.getEvent(roomId, replyEventId))?.sender === selfId;
+			// separate reply and new message
+			let previous: string[] = [];
+			let newBody: string[] = [];
+			let seenNonReply = false;
+			for (const line of body.split("\n")) {
+				if (seenNonReply || !line.startsWith("> ")) {
+					newBody.push(line);
+					seenNonReply = true;
+				} else previous.push(line.slice(2));
+			}
+
+			if (previous.some(s => s.toLowerCase().includes("summatia")) && !newBody.some(s => s.toLowerCase().includes("summatia"))) body = ""; // wipe body because this is not a real mention
+			else body = newBody.join("\n") + "\n\nIn reply to:\n" + previous.join("\n");
+		} catch (err) {
+			console.error(err);
+		}
 	}
 
 	if (body.includes(selfId) || body.toLowerCase().includes("summatia") || members == 2 && !name || replyToMe) {
@@ -51,6 +66,14 @@ client.on("room.message", async (roomId: string, event: RoomMessageEvent) => {
 		let platform: string;
 		if (members == 2 && !name) platform = "Matrix Direct Message";
 		else platform = `Matrix room "${name}" in space "${parents.reverse().join("/")}"`;
+		const matches = body.match(/<@[\w\d]+:[\w\d.]+>/g);
+		if (matches)
+			for (const match of matches) {
+				try {
+					const profile = await client.getUserProfile(match.slice(1, -1));
+					if (profile) body = body.replace(new RegExp(match.replace(/\./, "\\."), "g"), "@" + profile.displayname);
+				} catch (err) { }
+			}
 		let res = await chat((await client.getUserProfile(event.sender)).displayname, platform, { message: body }, false);
 		if (typeof res === "string") await client.replyText(roomId, event, res);
 		client.setTyping(roomId, false).catch(() => {}); // nobody cares if you can't set typing
