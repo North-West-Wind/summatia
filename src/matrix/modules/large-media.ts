51f99e7aac57a1d320f15ca3c45ebb6b7b1a6449
North-West-Wind <wsyn148@gmail.com>
2024-06-24 12:31:43 +0800

    proper mention user

--- src/matrix/modules/large-media.ts
+++ src/matrix/modules/large-media.ts
@@ -13,19 +13,28 @@ export default class LargeMediaModule extends SummatiaModule {
 		this.lastMxc = {};
 	}
 
+	async mentionUser(client: MatrixClient, userId: string) {
+		try {
+			if (userId) {
+				const profile = await client.getUserProfile(userId);
+				if (profile)
+					return `<a href="https://matrix.to/#/${encodeURIComponent(userId)}">@${profile.displayname}</a>`;
+			}
+		} catch (err) {
+			console.error(err);
+		}
+		return "Somebody";
+	}
+
 	async onMessage(client: MatrixClient, roomId: string, event: RoomMessageEvent) {
-		if (event.content?.msgtype === 'm.notice' && event.content.body.includes("Attachment is too large")) console.log("bridge bot say too large", roomId);
 		if (event.content?.msgtype === 'm.notice' && event.sender === process.env.MATRIX_BRIDGE_BOT && event.content.body.includes("Attachment is too large") && this.lastMxc[roomId]) {
-			
-			const msg = `${this.lastMxc[roomId].sender} sent an attachment too large for Discord!\n${process.env.MATRIX_HOMESERVER}/_matrix/media/r0/download/${this.lastMxc[roomId].url.slice(6)}`;
-			await client.sendText(roomId, msg);
+			const msg = `${await this.mentionUser(client, this.lastMxc[roomId].sender)} sent an attachment too large for Discord!\n${process.env.MATRIX_HOMESERVER}/_matrix/media/r0/download/${this.lastMxc[roomId].url.slice(6)}`;
+			await client.sendHtmlText(roomId, msg);
 			delete this.lastMxc[roomId];
-		} else if (event.content?.msgtype !== 'm.text' && (event.content as MediaEventContent).url) {
-			console.log("received non-text in room", roomId);
+		} else if (event.content?.msgtype !== 'm.text' && (event.content as MediaEventContent).url)
 			this.lastMxc[roomId] = {
 				sender: event.sender,
 				url: (event.content as MediaEventContent).url!
 			}
-		}
 	}
 }
