ab77da5429904431b139eeeabe05cbf46690059e
NorthWestWind <wsyn148@gmail.com>
2025-02-10 15:27:25 +0800

    debug

--- src/database-providers/splatoon3.ts
+++ src/database-providers/splatoon3.ts
@@ -26,8 +26,9 @@ export class Splatoon3DatabaseProvider extends SummatiaDatabaseProvider {
 	}
 
 	async setSubscriptions(id: string, manipulator: BitflagManipulator) {
-		const row = await this.get("SELECT id FROM s3subs WHERE id = ?", [id]);
-		if (row) {
+		const row = await this.get<{ id: string }>("SELECT id FROM s3subs WHERE id = ?", [id]);
+		console.log("Exist?", !!row?.id, "ID:", id, "Bitflag:", manipulator.bitflag);
+		if (row?.id) {
 			if (!manipulator.bitflag) await this.run("DELETE FROM s3subs WHERE id = ?", [id]);
 			else await this.run("UPDATE s3subs SET bitflag = ? WHERE id = ?", [manipulator.bitflag, id]);
 		} else if (manipulator.bitflag) await this.run("INSERT INTO s3subs VALUES (?, ?)", [id, manipulator.bitflag]);
