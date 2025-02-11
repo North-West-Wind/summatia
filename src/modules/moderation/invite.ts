53cadec46561beceb94e1e516c2c10e8a65cfa88
NorthWestWind <wsyn148@gmail.com>
2025-02-11 14:01:42 +0800

    mult factor with count minus uses

--- src/modules/moderation/invite.ts
+++ src/modules/moderation/invite.ts
@@ -1,6 +1,6 @@
 import { GuildMember, Snowflake } from "discord.js";
 import { Summatia } from "../../summatia";
-import { DiscordGuildMemberHandler, Startup, SummatiaListeners, SummatiaModule } from "..";
+import { DiscordGuildMemberHandler, SummatiaModule } from "..";
 
 const TRACK_WINDOW = 12 * 60 * 60 * 1000; // 12 hours in ms
 const FACTOR = 0.1;
@@ -14,11 +14,11 @@ export class InviteModerationModule extends SummatiaModule implements DiscordGui
 	}
 
 	async onGuildMemberAdd(_summatia: Summatia, member: GuildMember) {
-		const trackedUses = this.tracking.get(member.guild.id) || 0;
-		this.tracking.set(member.guild.id, trackedUses + 1);
+		const uses = this.tracking.get(member.guild.id) || 0;
+		this.tracking.set(member.guild.id, uses + 1);
 		setTimeout(() => this.tracking.set(member.guild.id, (this.tracking.get(member.guild.id) || 1) - 1), TRACK_WINDOW);
 
-		if (trackedUses >= member.guild.memberCount * FACTOR) {
+		if (uses >= (member.guild.memberCount - uses) * FACTOR) {
 			await member.guild.disableInvites();
 			const invites = await member.guild.invites.fetch();
 			for (const invite of invites.values())
